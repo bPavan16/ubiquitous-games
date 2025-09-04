2d9bf33adbf78f67cfe0e5ae81c3682001dff84f
bPavan16 <pavanbhakta163@gmail.com>
2025-09-04 19:52:31 +0530

    cors error fixedd

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ import GameFactory from './games/GameFactory.js';
 
 const app = express();
 const server = http.createServer(app);
+
+
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5174", "http://localhost:5173", "http://localhost:3000"],
+    origin: "*",
         methods: ["GET", "POST"],
         credentials: true
     }
